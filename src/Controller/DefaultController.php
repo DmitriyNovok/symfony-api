ae0797ad260c02221ea2a643883a9121f65eb860
DmitriyNovok <d.novokreshennyh@key-g.com>
2023-04-04 21:59:21 +0300

    Entity Repository Controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/hello')]
+    public function __construct(
+        private BookRepository $bookRepository,
+        private EntityManagerInterface $entityManager
+    ){}
+
+    #[Route('/books')]
     public function root(): Response
     {
-        return $this->json(['Hello' => 'World!!!']);
+        return $this->json(
+            $this->bookRepository->findAll()
+        );
     }
 }
