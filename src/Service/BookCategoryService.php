4609a00d9739e79d1e03bbb52426c224cf6afebf
DmitriyNovok <d.novokreshennyh@key-g.com>
2023-04-05 11:44:29 +0300

    Book category service

---
+++ src/Service/BookCategoryService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\BookCategory;
+use App\Model\BookCategoryListItem;
+use App\Model\BookCategoryListResponse;
+use App\Repository\BookCategoryRepository;
+use Doctrine\Common\Collections\Criteria;
+
+class BookCategoryService
+{
+    public function __construct(private BookCategoryRepository $bookCategoryRepository)
+    {
+    }
+
+    public function getCategories(): BookCategoryListResponse
+    {
+        $items = array_map(
+            fn (BookCategory $bookCategory) => new BookCategoryListItem(
+                $bookCategory->getId(), $bookCategory->getTitle(), $bookCategory->getSlug()
+            ),
+            $this->bookCategoryRepository->findBy([], ['title' => Criteria::ASC])
+        );
+
+        return new BookCategoryListResponse($items);
+    }
+}
