43cca627932c4aff4d475ba678be2230574d4939
DmitriyNovok <d.novokreshennyh@key-g.com>
2023-04-13 16:47:42 +0300

    Class ExceptionMappingResolver tests

---
+++ tests/Service/ExceptionHandler/ExceptionMappingResolverTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Tests\Service\ExceptionHandler;
+
+use App\Service\ExceptionHandler\ExceptionMappingResolver;
+use App\Tests\AbstractTestCase;
+
+class ExceptionMappingResolverTest extends AbstractTestCase
+{
+    public function testThrowsExceptionOnEmptyCode(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new ExceptionMappingResolver([
+            'someClass' => ['hidden' => true],
+        ]);
+    }
+
+    public function testResolveReturnsWhenNotFound(): void
+    {
+        $resolver = new ExceptionMappingResolver([]);
+
+        $this->assertNull(
+            $resolver->resolve(\InvalidArgumentException::class)
+        );
+    }
+
+    public function testResolvesClassItSelf(): void
+    {
+        $resolver = new ExceptionMappingResolver([
+            \InvalidArgumentException::class => ['code' => 400],
+        ]);
+
+        $mapping = $resolver->resolve(\InvalidArgumentException::class);
+
+        $this->assertEquals(400, $mapping->getCode());
+    }
+
+    public function testResolvesSubClass(): void
+    {
+        $resolver = new ExceptionMappingResolver([
+            \LogicException::class => ['code' => 500],
+        ]);
+
+        $mapping = $resolver->resolve(\LogicException::class);
+
+        $this->assertEquals(500, $mapping->getCode());
+    }
+
+    public function testResolvesHidden(): void
+    {
+        $resolver = new ExceptionMappingResolver([
+            \LogicException::class => [
+                'code' => 500,
+                'hidden' => false,
+            ],
+        ]);
+
+        $mapping = $resolver->resolve(\LogicException::class);
+
+        $this->assertFalse($mapping->isHidden());
+    }
+
+    public function testResolvesLoggable(): void
+    {
+        $resolver = new ExceptionMappingResolver([
+            \LogicException::class => [
+                'code' => 500,
+                'loggable' => true,
+            ],
+        ]);
+
+        $mapping = $resolver->resolve(\LogicException::class);
+
+        $this->assertTrue($mapping->isLoggable());
+    }
+}
