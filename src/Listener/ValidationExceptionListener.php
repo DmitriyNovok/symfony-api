98c8810302d53e8ad1c3f5b3a39d3815007a2bb6
DmitriyNovok <d.novokreshennyh@key-g.com>
2023-04-17 13:54:23 +0300

    Swagger ErrorResponse

--- src/Listener/ValidationExceptionListener.php
+++ src/Listener/ValidationExceptionListener.php
@@ -4,11 +4,13 @@ namespace App\Listener;
 
 use App\Exception\ValidationException;
 use App\Model\ErrorResponse;
+use App\Model\ErrorValidationDetails;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 class ValidationExceptionListener
 {
@@ -25,12 +27,26 @@ class ValidationExceptionListener
         }
 
         $data = $this->serializer->serialize(
-            new ErrorResponse($throwable->getMessage(), ['vaiolations' => $throwable->getViolations()]),
+            new ErrorResponse(
+                $throwable->getMessage(),
+                $this->formatViolations($throwable->getViolations())
+            ),
             JsonEncoder::FORMAT,
         );
 
         $event->setResponse(
             new JsonResponse($data, Response::HTTP_BAD_REQUEST, [], true)
         );
+    }
+
+    private function formatViolations(ConstraintViolationListInterface $constraintViolationList): ErrorValidationDetails
+    {
+        $details = new ErrorValidationDetails();
+
+        foreach ($constraintViolationList as $violation) {
+            $details->addViolations($violation->getPropertyPath(), $violation->getMessage());
+        }
+
+        return $details;
     }
 }
